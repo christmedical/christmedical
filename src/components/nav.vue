8ac5978e5e0fc740825e4f213875d8c218c52a62
Justin Loyed <justin.loyed@gmail.com>
2017-08-28 22:50:22 -0400

    Added nav template

---
+++ src/components/nav.vue
@@ -0,0 +1,12 @@
+<template>
+  <div id="sidebar-wrapper">
+    <nav v-bind:class="active" v-on:click.prevent>
+      <a href="#" class="home" v-on:click="makeActive('home')">Home</a>
+      <a href="#" class="patient" v-on:click="makeActive('patient')">Patient</a>
+      <a href="#" class="followups" v-on:click="makeActive('followups')">Follow Ups</a>
+      <a href="#" class="reports" v-on:click="makeActive('reports')">Reports</a>
+      <a href="#" class="sync" v-on:click="makeActive('sync')">Sync</a>
+      <a href="#" class="settings" v-on:click="makeActive('settings')">Settings</a>
+    </nav>
+  </div>
+</template>
